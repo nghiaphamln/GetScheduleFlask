3cac4444e7dd593f755382d7183cee8cca17a0c6
NghiaPH <nghiaphamln3@gmail.com>
2020-09-04 15:59:08 +0700

    add Bypass Captcha

--- BypassCaptcha.py
+++ BypassCaptcha.py
@@ -1,21 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
+#Soup
 url = 'https://dkmh.tdmu.edu.vn/'
-
-# Get Captcha
 dataHtml = requests.get(url)
 data = BeautifulSoup(dataHtml.content,'html.parser')
+
+# Get Captcha
 captcha = data.find('span', id='ctl00_ContentPlaceHolder1_ctl00_lblCapcha').text
 
+# Get __VIEWSTATE
+viewstate = data.find('input', id='__VIEWSTATE')
+viewstate = re.search(" +value=\"(.*?)\"", str(viewstate))
+viewstate = viewstate.group(1)
 
-#Bypass Captcha
+# Get __VIEWSTATEGENERATOR
+viewstategenerator = data.find('input', id='__VIEWSTATEGENERATOR')
+viewstategenerator = re.search(" +value=\"(.*?)\"", str(viewstategenerator))
+viewstategenerator = viewstategenerator.group(1)
+
+# Bypass Captcha
 values = {
-    'ctl00$ContentPlaceHolder1$ctl00$txtCaptcha': 'MFJ41',
-    'ctl00$ContentPlaceHolder1$ctl00$btnXacNhan': 'Vào website',
-    '__VIEWSTATEGENERATOR': 'CA0B0334'
+    '__VIEWSTATE': viewstate,
+    '__VIEWSTATEGENERATOR': viewstategenerator,
+    'ctl00$ContentPlaceHolder1$ctl00$txtCaptcha': captcha,
+    'ctl00$ContentPlaceHolder1$ctl00$btnXacNhan': 'Vào website'
 }
-urlPost = 'https://dkmh.tdmu.edu.vn'
-r = requests.post(urlPost, data=values)
-dataCaptcha = BeautifulSoup(r.content,'html.parser')
-print(dataCaptcha)
+def ExcuteBypassCaptcha():
+    return requests.post(url, data = values)
